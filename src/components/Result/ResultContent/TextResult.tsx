d812342ef02a15b8e971993c9e89b40e70890bd3
Pratik <pratikmndh@gmail.com>
2023-02-15 10:39:45 -0500

    fix: temp fix to delay visibility of keypoints

--- src/components/Result/ResultContent/TextResult.tsx
+++ src/components/Result/ResultContent/TextResult.tsx
@@ -13,30 +13,41 @@ const TextResult = ({ textSummaryResponse }: TextResultPropType) => {
         <div className="flex items-center justify-between">
           <h3 className="text-xl font-bold">Summary</h3>
           <div className="hidden md:block">
-            <CopyButton textToCopy={textSummaryResponse?.summary} />
+            <CopyButton textToCopy={textSummaryResponse.summary} />
           </div>
         </div>
-        <p className="my-8 text-base leading-6 md:leading-8">{textSummaryResponse?.summary}</p>
-        <div className="block md:hidden">
-          <CopyButton textToCopy={textSummaryResponse?.summary} />
-        </div>
-        <div className="mt-16 flex items-center justify-between">
-          <h3 className="text-xl font-bold">Key Points & Analysis</h3>
-          <div className="hidden md:block">
-            <CopyButton textToCopy={textSummaryResponse?.keyPoints?.join("\n")} />
-          </div>
-        </div>
-        <ul className="my-8 list-disc px-6 text-base leading-6 md:leading-8">
-          {textSummaryResponse.keyPoints?.map((keyPoint, id) => (
-            <li key={id}>{keyPoint}</li>
-          ))}
-        </ul>
-        <div className="mb-4 text-base leading-6 md:leading-8">
-          This articles has a {textSummaryResponse.bias} bias and {textSummaryResponse.tone} tone
-        </div>
-        <div className="block md:hidden">
-          <CopyButton textToCopy={textSummaryResponse?.keyPoints?.join("\n")} />
-        </div>
+        <p className="my-8 text-base leading-6 md:leading-8">{textSummaryResponse.summary}</p>
+        {textSummaryResponse.keyPoints?.length > 0 && (
+          <>
+            <div className="block md:hidden">
+              <CopyButton textToCopy={textSummaryResponse.summary} />
+            </div>
+            <div className="mt-16 flex items-center justify-between">
+              <h3 className="text-xl font-bold">Key Points & Analysis</h3>
+              <div className="hidden md:block">
+                <CopyButton
+                  textToCopy={textSummaryResponse.keyPoints?.join("\n")}
+                />
+              </div>
+            </div>
+            <ul className="my-8 list-disc px-6 text-base leading-6 md:leading-8">
+              {textSummaryResponse.keyPoints?.map((keyPoint, id) => (
+                <li key={id}>{keyPoint}</li>
+              ))}
+            </ul>
+          </>
+        )}
+
+        {textSummaryResponse.bias && textSummaryResponse.tone && (
+          <>
+            <div className="mb-4 text-base leading-6 md:leading-8">
+              This article has a {textSummaryResponse.bias} bias and {textSummaryResponse.tone} tone
+            </div>
+            <div className="block md:hidden">
+              <CopyButton textToCopy={textSummaryResponse.keyPoints?.join("\n")} />
+            </div>
+          </>
+        )}
       </div>
     </Container>
   );
