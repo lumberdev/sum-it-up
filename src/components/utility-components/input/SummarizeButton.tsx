2411897827870d71d8d850290e84214f9862af0a
Pratik <pratikmndh@gmail.com>
2023-02-22 00:42:36 -0500

    style: add hover effect to summarize button

--- src/components/utility-components/input/SummarizeButton.tsx
+++ src/components/utility-components/input/SummarizeButton.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 const SummarizeButton = ({ children, className = "" }: Props) => {
   return (
     <button
-      className={`mx-auto flex h-20 min-w-[14rem] items-center justify-center rounded-full bg-primary text-base font-bold uppercase text-white md:h-24 md:text-lg ${className}`}
+      className={`mx-auto flex h-20 min-w-[14rem] items-center justify-center rounded-full bg-primary text-base font-bold uppercase text-white transition duration-300 hover:bg-primary/95 hover:ring-2 md:h-24 md:text-lg ${className}`}
       type="submit"
     >
       {children}
--- tailwind.config.js
+++ tailwind.config.js
@@ -48,10 +48,6 @@ module.exports = {
           "100%": { opacity: 1 },
         },
       },
-      transitionTimingFunction: {
-        "in-expo": "cubic-bezier(0.95, 0.05, 0.795, 0.035)",
-        "out-expo": "cubic-bezier(0.19, 1, 0.22, 1)",
-      },
     },
   },
   plugins: [],
