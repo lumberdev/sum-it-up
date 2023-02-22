6fa171d0b12c8691bc44481092612f279c25fa55
Pratik <pratikmndh@gmail.com>
2023-02-21 23:11:18 -0500

    style: add transition to togglers

--- src/components/utility-components/result/ResultPageContentToggler.tsx
+++ src/components/utility-components/result/ResultPageContentToggler.tsx
@@ -6,23 +6,28 @@ type ResultPageContentTogglerPropType = {
 };
 const ResultPageContentToggler = ({ resultPageContent, setResultPageContent }: ResultPageContentTogglerPropType) => {
   return (
-    <div className="mx-auto inline-flex h-16 w-full max-w-[19rem] flex-1 items-center justify-center rounded-full bg-primary p-2 text-white md:h-[4.5rem] md:max-w-[26rem]">
+    <div className="relative mx-auto inline-flex h-16 w-full max-w-[19rem] flex-1 items-center justify-center rounded-full bg-primary p-2 text-white md:h-[4.5rem] md:max-w-[26rem]">
       <button
-        className={`h-full flex-1 rounded-full text-sm font-bold uppercase transition ${
-          resultPageContent === "summary" && " bg-white text-primary"
+        className={`z-10 h-full flex-1 rounded-full text-sm font-bold uppercase transition ${
+          resultPageContent === "summary" && "  text-primary"
         } ${resultPageContent !== "summary" && "hover:bg-white/10"}`}
         onClick={() => setResultPageContent("summary")}
       >
         Summary
       </button>
       <button
-        className={`h-full flex-1 rounded-full text-sm font-bold uppercase transition ${
-          resultPageContent === "original" && " bg-white text-primary"
+        className={`z-10 h-full flex-1 rounded-full text-sm font-bold uppercase transition ${
+          resultPageContent === "original" && "  text-primary"
         } ${resultPageContent !== "original" && "hover:bg-white/10"}`}
         onClick={() => setResultPageContent("original")}
       >
         Original
       </button>
+      <span
+        className={`z-1 absolute left-2 right-auto h-[calc(100%-1rem)] w-[calc(50%-0.5rem)] rounded-full bg-white transition-all ease-in-out ${
+          resultPageContent === "original" && "left-1/2 right-2"
+        }`}
+      ></span>
     </div>
   );
 };
