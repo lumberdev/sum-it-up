a39a3fc452686ffd57b670835e5af8283441f118
Maia Sason (work) <maia@lumber.dev>
2023-02-23 14:41:49 -0500

    fix: push up compression

--- src/utils/generateLinkToShare.ts
+++ src/utils/generateLinkToShare.ts
@@ -1,9 +1,10 @@
+import LZString from "lz-string";
 import { ResponseType } from "~/types";
 
 export function encodeStateToUrl(originalContent: string, responseObject: ResponseType): string {
   const baseUrl = process.env.BASE_URL || window.location.origin;
-  const queryString = `?original=${encodeURIComponent(originalContent)}&result=${encodeURIComponent(
-    JSON.stringify(responseObject),
-  )}`;
+  const compressedOriginal = LZString.compressToEncodedURIComponent(originalContent);
+  const compressedResult = LZString.compressToEncodedURIComponent(JSON.stringify(responseObject));
+  const queryString = `?original=${compressedOriginal}&result=${encodeURIComponent(compressedResult)}`;
   return baseUrl + queryString;
 }
