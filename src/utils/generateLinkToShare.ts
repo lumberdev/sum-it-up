851d585177ba2a5938fbe3c484400e3bb1f8b21a
Maia Sason (work) <maia@lumber.dev>
2023-03-02 15:34:31 -0500

    fix: songdetail param

--- src/utils/generateLinkToShare.ts
+++ src/utils/generateLinkToShare.ts
@@ -4,6 +4,6 @@ export function encodeStateToUrl(originalContent: string, responseObject: Respon
   const baseUrl = process.env.BASE_URL || window.location.origin;
   const queryString = `?result=${encodeURIComponent(JSON.stringify(responseObject))}&original=${encodeURIComponent(
     originalContent,
-  )}${songDetails && `&songDetails=${encodeURIComponent(songDetails)}`}`;
+  )}${songDetails ? `&songDetails=${encodeURIComponent(songDetails)}` : ""}`;
   return baseUrl + queryString;
 }
