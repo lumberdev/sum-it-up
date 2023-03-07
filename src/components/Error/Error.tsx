def55233f85672a9a802955789fa0571b0b104ee
Rhut Virani <19146537+Rhut-virani@users.noreply.github.com>
2023-03-07 14:11:08 +0530

    fixed Error page reset

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -4,14 +4,10 @@ import Container from "../utility-components/Container";
 import errorIcon from "../../assets/error.png";
 import ResultPageHeader from "../Result/ResultPageHeader";
 
-const Error = ({ reset, setIsError }: { reset: () => void; setIsError: (arg0: boolean) => void }) => {
-  const handleErrorNewSearch = () => {
-    setIsError(false);
-    reset();
-  };
+const Error = ({ handleNewSearchBtnClick }: { handleNewSearchBtnClick: () => void }) => {
   return (
     <>
-      <ResultPageHeader handleNewSearchBtnClick={handleErrorNewSearch} />
+      <ResultPageHeader handleNewSearchBtnClick={handleNewSearchBtnClick} />
       <Container>
         <div className="mx-auto mb-8 max-w-[75rem] animate-fadeIn rounded-[20px] border-2 border-primary bg-white py-12 px-8 md:my-20 md:p-20">
           <p className="text-center text-[1.5rem] font-bold">Sorry, Something went wrong</p>
